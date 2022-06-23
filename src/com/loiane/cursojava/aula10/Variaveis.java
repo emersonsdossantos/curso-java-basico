0d043d47189a312e010b9d2c98bb6e38192ea270
Emerson <emersonsdossantos@gmail.com>
2022-06-22 21:22:41 -0300

    subindo projeto

---
+++ src/com/loiane/cursojava/aula10/Variaveis.java
@@ -0,0 +1,16 @@
+package com.loiane.cursojava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		int idade = 37;
+		String nome = "Emerson";
+		String nomeDoCachorro = "Baruk";
+		
+		System.out.println("Nome: " + nome);
+		System.out.println("Idade: " + idade + " anos");
+		System.out.println("Nome do cachorro: "+ nomeDoCachorro); 
+	}
+
+}
