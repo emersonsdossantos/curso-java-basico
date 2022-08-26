91602c9f872e8ba85f2ea389de8f0115caa18516
Emerson <emersonsdossantos@gmail.com>
2022-08-25 21:53:04 -0300

    Aula 13 - Exercício 01

---
+++ src/com/loiane/cursojava/aula13/exercicios/Exe01.java
@@ -0,0 +1,12 @@
+package com.loiane.cursojava.aula13.exercicios;
+
+//Faca um Programa que mostre a mensagem "Ala mundo" na tela.
+
+public class Exe01 {
+		
+	public static void main(String[] args) {
+		
+		System.out.println("Ola Mundo");
+	}
+
+}
